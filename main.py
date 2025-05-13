571f1a30b180d541e00350c08cb6d31fa70b5bb2
bualat-benchbrian <bualatbenchbrian@gmail.com>
2025-05-13 18:03:36 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from pykachupackage import bualat
+
+def main():
+    while True:
+        print("\nMain Menu")
+        print("1. besa module")
+        print("2. bualat module")
+        print("3. maestre module")
+        print("4. salespara module")
+        print("5. serquina module")
+        print("6. exit")
+        choice = input("Enter your choice: ")
+        match choice:
+            case '1':
+                print("\nYou selected besa module")
+            case '2':
+                print("\nYou selected bualat module")
+                bualat.hero_num()
+                bualat.hero_name()
+            case '3':
+                print("\nYou selected maestre module")
+            case '4':
+                print("\nYou selected salespara module")
+            case '5':
+                print("\nYou selected serquina module")
+            case '6':
+                print("Thank you for using the program. Goodbye!")
+                exit()
+            case _:
+                print("Invalid choice, please try again.")
+
+main()
