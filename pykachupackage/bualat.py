9ddd6ebba14d70d1e9039c4028cbb4a1aae0bab8
bualat-benchbrian <bualatbenchbrian@gmail.com>
2025-05-13 17:59:03 +0800

    create package

---
+++ pykachupackage/bualat.py
@@ -0,0 +1,11 @@
+import random
+import emoji
+from getname import random_name
+
+def hero_num():
+    hero_number = random.randint(1, 100)
+    print(emoji.emojize (f"Your hero number is: {hero_number} :dizzy:"))
+
+def hero_name():
+    hero_alias = random_name('superhero')
+    print(emoji.emojize(f"Your hero nickname is: {hero_alias} :khanda:"))
---
+++ pykachupackage/salespara.py
@@ -0,0 +1,10 @@
+import pyjokes
+
+def give_joke():
+    name = input("Enter your name: ")
+    joke = pyjokes.get_joke()
+
+    print(f"\nHello, {name}!")
+    print(f"Here's a joke for you: ")
+    print(f"\n {joke}")
+    print (f"\nI hope made you giggle, {name}! <3 \n")
