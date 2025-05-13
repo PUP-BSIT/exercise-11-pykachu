53a6ab4ea3991e6602924a1065607e806f0a3195
besa-vinceadrian <vinceadrianbesa@gmail.com>
2025-05-13 19:47:09 +0800

    fix spaces and indentation

--- pykachupackage/besa.py
+++ pykachupackage/besa.py
@@ -4,5 +4,6 @@ import pytz
 def show_manila_time():
     manila = pytz.timezone('Asia/Manila')
     time_now = datetime.now(manila)
+    
     print(f"Besa's date and time in Manila, Philippines: {
             time_now.strftime('%Y-%m-%d %H:%M:%S')}")
